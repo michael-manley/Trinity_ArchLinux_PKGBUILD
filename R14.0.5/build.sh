bcf3ceb3ff6737c6a42f9c3f56b244bcc3b51e7c
Michael Manley <mmanley@ntge.net>
2019-01-30 10:59:41 -0800

    Added R14.0.5 PKGBUILDs

--- R14.0.5/build.sh
+++ R14.0.5/build.sh
@@ -1,24 +1,3 @@
-# Trinity_ArchLinux_PKGBUILD
-PKGBUILD's for building Trinity on Arch Linux
-
-## Before installing
-Before installing please remove any presence of JRE and RUBY packages, 
-after install you can put them back.
-
-
-## How to build Trinity with these Files
-If you have trouble building one of these packages,
-please source the environment variables by issuing (from package directory):
-```
-. ../environment
-```
-OR
-```
-source ../environment
-```
-
-This script will get you to a basic Trinity environment
-```
 #!/bin/bash
 set -e
 tdesrc=$PWD
@@ -57,10 +36,3 @@ for pkg in tdesdk tdevelop tdewebdev; do
   cd "$dir"/tde-"$pkg"
   makepkg -Lsci
 done
-```
-
-## Unbuildable Packages
-Here lists the unbuildable packages at the moment. Please help make these packages buildable and submit any pull
-requests to fix them. Each package has a Bug entry which includes the build log for reference.
-
-1. tde-tdeartwork - Fails with undefined references see Issue #19
