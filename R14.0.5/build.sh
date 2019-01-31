328c69637d912675c85f70e1fcd9c0b58e588497
Michael Manley <mmanley@ntge.net>
2019-01-31 12:07:03 -0800

    Final Packages

--- R14.0.5/build.sh
+++ R14.0.5/build.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
+
+### !!! tdeartwork in tde-base removed due to no compile
+### !!! amarok in tde-extra removed due to no compile
+### !!! gtk3-tqt-engine in tde-extra removed due to no compile
+### !!! kmplayer in tde-extra removed due to no compile
+### !!! systemsettings in tde-extra removed due to no compile
+### !!! tdeedu in tde-extra removed due to no compile
+
 set -e
 tdesrc=$PWD
 
 cd $tdesrc/tde-core
 dir=$PWD
-for pkg in tde-tqt3 tde-tqtinterface tde-arts tde-dbus-tqt tde-dbus-1-tqt tde-tqca-tls tde-libart-lgpl tde-avahi-tqt tde-tdelibs tde-tdebase; do
-  cd "$dir"/tde-"$pkg"
-  makepkg -Lsci
-done
-
-cd $tdesrc/tde-base
-dir=$PWD
-for pkg in tdeadmin tdeartwork tdebindings tdegraphics tdeutils; do
-  cd "$dir"/tde-"$pkg"
-  makepkg -Lsci
+for pkg in tqt3 tqtinterface arts dbus-tqt dbus-1-tqt tqca-tls libart-lgpl avahi-tqt tdelibs tdebase; do
+ cd "$dir"/tde-"$pkg"
+ makepkg -Lsci
 done
 
 cd $tdesrc/tde-libs
 dir=$PWD
 for pkg in libcaldav libcarddav libkdcraw libkexiv2; do
-  cd "$dir"/tde-"$pkg"
-  makepkg -Lsci
+ cd "$dir"/tde-"$pkg"
+ makepkg -Lsci
+done
+
+cd $tdesrc/tde-base
+dir=$PWD
+for pkg in tdeadmin tdebindings tdegraphics tdeutils; do
+ cd "$dir"/tde-"$pkg"
+ makepkg -Lsci
 done
 
 cd $tdesrc/tde-extra
 dir=$PWD
-for pkg in tdenetwork tdegames amarok gtk-qt-engine gtk3-tqt-engine kmplayer krusader ksplash-engine-moodin style-qtcurve systemsettings tdeaccessability tdeaddons tdeedu tdemultimedia tdenetworkmanager tdepim tdepowersave tdesudo tdetoys tdmtheme twin-style-crystal; do
+for pkg in tdenetwork tdegames gtk-qt-engine tdemultimedia krusader ksplash-engine-moodin style-qtcurve tdeaccessibility tdepim tdeaddons tdenetworkmanager tdepowersave tdesudo tdetoys tdmtheme twin-style-crystal; do
   cd "$dir"/tde-"$pkg"
   makepkg -Lsci
 done
@@ -36,3 +44,6 @@ for pkg in tdesdk tdevelop tdewebdev; do
   cd "$dir"/tde-"$pkg"
   makepkg -Lsci
 done
+
+mkdir -p $tdesrc/out
+mv $(find $tdesrc -name *.pkg.tar.xz) $tdesrc/out
